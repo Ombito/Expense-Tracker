d87d6192f2a8b02c8d7148ceab9dc20ac7af5a3d
ombito <alvinombito09@gmail.com>
2023-10-04 00:12:35 +0300

    Feat: add menu options

--- expense.py
+++ expense.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column, Integer, String, create_engine, Date, Float, func
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine, Date, Float, func, ForeignKey, Table
 from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 engine = create_engine("sqlite:///my_database.db")
 
-    #create expenses table
-class Transaction(Base):
+expense_category = Table(
+    'expense_category', 
+    Base.metadata,
+    Column('expense_id', Integer, ForeignKey('expenses.id')),
+    Column('category_id', Integer, ForeignKey('categories.id'))
+)
+
+# Create expenses table
+class Expense(Base):
     __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True)
@@ -14,29 +21,49 @@ class Transaction(Base):
     category = Column(String())
     description = Column(String())
     amount = Column(Float())
-    
+
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    user = relationship('User', back_populates='expenses')
+    categories = relationship('Category', secondary=expense_category)
 
     def __repr__(self):
         return f"Expense: {self.amount}"
-    
-class User(Base):
-    __table__name__ = "users"
 
-    id = Column(Integer(), primary_key = True)
+# Create a user table
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer(), primary_key=True)
     username = Column(String())
+    password = Column(String()) 
     email = Column(String())
 
+    expenses = relationship('Expense', back_populates='user')
 
+# Create a category table
 class Category(Base):
-    __table__name__ = "categories"
+    __tablename__ = "categories"
 
-    id = Column(Integer(), primary_key = True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String())
     description = Column(String())
+
+    expenses = relationship('Expense', secondary='expense_category')
+
+def seed_initial_data():
+    # Create some initial users
+    user1 = User(username='alvin', email='alvin@gmail.com', password='password') 
+    user2 = User(username='grace', email='grace@gmail.com', password='password')  
+
     
-
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-    session = sessionmaker()(bind=engine)
+    session.add_all([user1, user2])
     session.commit()
+
+
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+seed_initial_data()
--- index.py
+++ index.py
@@ -1,9 +1,69 @@
-from expense  import User, Expense, Category, session, Base
+from expense  import User, Expense, Category, engine, sessionmaker
+
+Session = sessionmaker(bind=engine)
+
+def add_expense():
+    username = input("Enter your user ID: ")
+    date = input("Enter the date of transaction - (YYYY-MM-DD): ")
+    description = input("Enter the description of the expense: ")
+    amount = float(input("Enter amount: "))
+
+       
+    session = Session()
+
+        # Check if the user with the given user_id exists
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        new_expense = Expense(username=username, date=date, description=description,amount=amount)
+        session.add(new_expense)
+        session.commit()
+        print("Expense added successfully.")
+    else:
+        print("User not found. Please enter a valid user ID.")
+
+
+def view_expenses():
+    username = input("Enter your username: ")
+    session = Session()
+
+        # Check if the user with the given user_id exists
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        expenses = session.query(Expense).filter_by(user_id=user.user_id).all()
+        if expenses:
+            print("\nExpense List: ")
+            for expense in expenses:
+                print(f"Date: {expense.date}, Description: {expense.description}, Amount: {expense.amount}")
+        else:
+            print("No expenses found for this user.")
+    else:
+        print("User not found. Please enter a valid user ID.")
+
+
+
+def delete_expense():
+    username = input("Enter your username: ")
+    expense_id = int(input("Enter the ID of the expense you want to delete: "))
+
+    session = Session()
+
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        expense = session.query(Expense).filter_by(expense_id=expense_id, username=username).first()
+        if expense:
+            session.delete(expense)
+            session.commit()
+            print("Expense deleted successfully.")
+        else:
+            print("Expense not found for this user.")
+    else:
+        print("User not found. Please enter a valid user ID.")
 
 
 def main_menu():
     while True:
         print("Select an option...\n")
+        print("0. Sign-In")
         print("1. Enter a new expense")
         print("2. View expenses")
         print("3. Delete expense")
@@ -11,24 +71,30 @@ def main_menu():
 
         choice = int(input())
 
+        session = Session()
+
+
         if choice == 1:
-            date = input("Enter the date of transaction - (YYYY-MM-DD): ")
-            description = input("Enter the description of the expense: ")
-            amount = float(input("Enter amount: "))
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
 
-            categories = [ "Transport", "Savings", "Food", "Clothing", "Entertainment", "Rent", "Airtime", "Travel" ]
+            # Check if the entered username and password match a user in the database
+            user = session.query(User).filter_by(username=username, password=password).first()
 
-            while True:
-                for i, category_name in enumerate(categories):
-                    print(f" {i + 1}. {category_name}")
-                break
+            if user:
+                print("Sign-in successful. Welcome,", {username})
+            else:
+                print("Sign-in failed. Please check your username and password.")
+
+
 
         elif choice == 2:
             print("Select a category... ")
-            input("Enter the category: ")
+            view_expenses()
 
         elif choice == 3:
             print("Select an expense... " )
+            delete_expense()
 
         elif choice == 4:
             print("Goodbye!")
@@ -37,4 +103,5 @@ def main_menu():
         else:
             print("Invalid input. Please enter a valid number.")
 
-main_menu()
+if __name__ == "__main__":
+    main_menu()
