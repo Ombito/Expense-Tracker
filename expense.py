22eb40deb23bb52cbed480bf9564a01c57a30544
ombito <alvinombito09@gmail.com>
2023-10-03 22:18:27 +0300

    Add database table

--- expense.py
+++ expense.py
@@ -1,13 +1,21 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base
 engine = create_engine("sqlite:///my_database.db")
 
+    #create expenses table
 class Transaction(Base):
-    __tablename__ = "transactions"
+    __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True)
-    date = Column(datetime, server_default=func.now())
+    date = Column(DateTime, server_default=func.now())
     category = Column(String())
+    amount = Column(Integer())
+    description = Column(String())
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    session = sessionmaker()(bind=engine)
+    session.commit()
